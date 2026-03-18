56f7ff04a18b514b3f8db9b1e2fa0a538f4f2cc3
Hash-Arnav <arnavalld28@gmail.com>
2026-03-18 13:23:59 +0530

    UC5: Inline array initialization using String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,61 +1,19 @@
-
-
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        // Letter O
-        String[] O = {
-                "  *****  ",
-                " *     * ",
-                "*       *",
-                "*       *",
-                "*       *",
-                " *     * ",
-                "  *****  "
+        // Inline array initialization using String.join()
+        String[] lines = {
+            String.join(" ", " *** ", " *** ", " ***** ", " ***** "),
+            String.join(" ", "*   *", "*   *", "*     ", "*     "),
+            String.join(" ", "*   *", "*   *", "***** ", "***** "),
+            String.join(" ", "*   *", "*   *", "*     ", "    * "),
+            String.join(" ", " *** ", " *** ", " ***** ", "***** ")
         };
 
-        // Letter P
-        String[] P = {
-                " ******  ",
-                " *     * ",
-                " *     * ",
-                " ******  ",
-                " *       ",
-                " *       ",
-                " *       "
-        };
-
-        // Letter S
-        String[] S = {
-                " ****** ",
-                " *      ",
-                " *      ",
-                " *****  ",
-                "      * ",
-                "      * ",
-                " ****** "
-        };
-
-        String word = "OOPS";
-
-        // Loop through rows
-        for (int i = 0; i < 7; i++) {
-
-            // Loop through characters
-            for (char ch : word.toCharArray()) {
-
-                if (ch == 'O') {
-                    System.out.print(O[i] + " ");
-                } else if (ch == 'P') {
-                    System.out.print(P[i] + " ");
-                } else if (ch == 'S') {
-                    System.out.print(S[i] + " ");
-                }
-            }
-
-            // Move to next line
-            System.out.println();
+        // Print using for-each loop
+        for (String line : lines) {
+            System.out.println(line);
         }
     }
 }
